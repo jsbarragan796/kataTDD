b3c7ef530c5dddd8f3fbe1054be07104b8a5d3ef
juanSebastian <js.barragan796@uniandes.edu.co>
2018-03-18 21:44:31 -0500

    I2-P4 refactor test string n elementos numero y min de los elementos(azul)

--- estadisticTest.py
+++ estadisticTest.py
@@ -12,15 +12,11 @@ class EstadisticTest(TestCase):
         self.assertEqual(Estadistic().elements("1,1"), [2], " Two String numbers")
         self.assertEqual(Estadistic().elements("1"), [1], " One String numbers")
         self.assertEqual(Estadistic().elements("1,4,6,2,6,32,1"), [7], " Seven String numbers")
-    def test_minElement_stringNoneElement(self):
-        self.assertEqual(Estadistic().minElement(""),[0, None],"Empty string")
-    def test_minElement_stringOneElement(self):
-        self.assertEqual(Estadistic().minElement("1"),[1,1],"One String numbers")
+    def test_minElement_stringNElement(self):
+        self.assertEqual(Estadistic().minElement(""), [0, None], "Empty string")
+        self.assertEqual(Estadistic().minElement("1"), [1, 1], "One String numbers")
         self.assertEqual(Estadistic().minElement("10"), [1, 10], "One String numbers")
-    def test_minElement_stringTwoElement(self):
         self.assertEqual(Estadistic().minElement("4,2"),[2,2],"Two String numbers")
         self.assertEqual(Estadistic().minElement("10,1200"), [2, 10], "Two String numbers")
-
-    def test_minElement_stringNElement(self):
         self.assertEqual(Estadistic().minElement("4,2,93,7,24,7,3,12"),[8,2],"n String numbers")
         self.assertEqual(Estadistic().minElement("10,12,892,23591,8"), [5, 8], "Two String numbers")
