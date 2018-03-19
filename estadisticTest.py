f2932809f3f7bba426281ca2c3a9667f9d64b2a7
juanSebastian <js.barragan796@uniandes.edu.co>
2018-03-18 21:08:55 -0500

    Iteracion2-Paso1 test string vacio numero y minimo de lo elementos (rojo)

--- estadisticTest.py
+++ estadisticTest.py
@@ -5,10 +5,12 @@ __author__ = 'Juan Sebastian Barragan'
 from Estadistic import Estadistic
 
 class EstadisticTest(TestCase):
-    def test_nElements_stringNElementos(self):
+    def test_nElements_stringNElement(self):
         self.assertEqual(Estadistic().elements(" "),[0],"Empty string")
         self.assertEqual(Estadistic().elements(""), [0], "Empty string")
         self.assertEqual(Estadistic().elements("1,1,3,2"), [4], " Four String numbers")
         self.assertEqual(Estadistic().elements("1,1"), [2], " Two String numbers")
         self.assertEqual(Estadistic().elements("1"), [1], " One String numbers")
         self.assertEqual(Estadistic().elements("1,4,6,2,6,32,1"), [7], " Seven String numbers")
+    def test_minElement_stringOneElement(self):
+        self.assertEqual(Estadistic().minElement(""),[0,None],"Empty string")
