02e54f15db3ccf8bea8aa02017471a1edabb7c8c
juanSebastian <js.barragan796@uniandes.edu.co>
2018-03-18 21:53:04 -0500

    I2-P4 refactor  test y clase (azul)

--- Estadistic.py
+++ Estadistic.py
@@ -5,20 +5,12 @@ import numpy as np
 #stringNumbers = raw_input('Type your numbers separated by commas')
 
 class Estadistic:
-    def elements(self, string):
-        if string == " " or string == "":
-            return [0]
-        elif "," in string:
-            numbers = string.split(",")
-            return [len(numbers)]
-        else:
-            return [1]
+
     def minElement(self,string):
-        resp = self.elements(string)
         if string == " " or string == "":
-            return [resp[0], None]
+            return [0, None]
         elif "," in string:
             numbers = np.array(string.split(",")).astype(np.int)
-            return [resp[0],min(numbers)]
+            return [len(numbers),min(numbers)]
         else:
-            return [resp[0],int(string)]
+            return [1,int(string)]
--- estadisticTest.py
+++ estadisticTest.py
@@ -5,13 +5,7 @@ __author__ = 'Juan Sebastian Barragan'
 from Estadistic import Estadistic
 
 class EstadisticTest(TestCase):
-    def test_nElements_stringNElement(self):
-        self.assertEqual(Estadistic().elements(" "),[0],"Empty string")
-        self.assertEqual(Estadistic().elements(""), [0], "Empty string")
-        self.assertEqual(Estadistic().elements("1,1,3,2"), [4], " Four String numbers")
-        self.assertEqual(Estadistic().elements("1,1"), [2], " Two String numbers")
-        self.assertEqual(Estadistic().elements("1"), [1], " One String numbers")
-        self.assertEqual(Estadistic().elements("1,4,6,2,6,32,1"), [7], " Seven String numbers")
+
     def test_minElement_stringNElement(self):
         self.assertEqual(Estadistic().minElement(""), [0, None], "Empty string")
         self.assertEqual(Estadistic().minElement("1"), [1, 1], "One String numbers")
@@ -19,4 +13,4 @@ class EstadisticTest(TestCase):
         self.assertEqual(Estadistic().minElement("4,2"),[2,2],"Two String numbers")
         self.assertEqual(Estadistic().minElement("10,1200"), [2, 10], "Two String numbers")
         self.assertEqual(Estadistic().minElement("4,2,93,7,24,7,3,12"),[8,2],"n String numbers")
-        self.assertEqual(Estadistic().minElement("10,12,892,23591,8"), [5, 8], "Two String numbers")
+        self.assertEqual(Estadistic().minElement("10,12,892,23591,8"), [5, 8], "n String numbers")
