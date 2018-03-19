caad05f3d657613b8b61a22a05c3b507080bb8b4
juanSebastian <js.barragan796@uniandes.edu.co>
2018-03-18 21:53:42 -0500

    I3-P1 crear test string 1 elemento num,min max  de los elementos (rojo)

--- estadisticTest.py
+++ estadisticTest.py
@@ -14,3 +14,6 @@ class EstadisticTest(TestCase):
         self.assertEqual(Estadistic().minElement("10,1200"), [2, 10], "Two String numbers")
         self.assertEqual(Estadistic().minElement("4,2,93,7,24,7,3,12"),[8,2],"n String numbers")
         self.assertEqual(Estadistic().minElement("10,12,892,23591,8"), [5, 8], "n String numbers")
+    def test_minMaxElement_stringNoneElement(self):
+        self.assertEqual(Estadistic().minMaxElement(" "), [0, None, None], "Empty string")
+        self.assertEqual(Estadistic().minMaxElement(""), [0, None, None], "Empty string")
