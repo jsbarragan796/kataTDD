0eea0760587e61203823644691d6fdb94e1a25fb
juanSebastian <js.barragan796@uniandes.edu.co>
2018-03-18 22:16:55 -0500

    I4-P2 crear test string 1 elemento num,min,max prom (rojo)

--- estadisticTest.py
+++ estadisticTest.py
@@ -9,8 +9,8 @@ class EstadisticTest(TestCase):
     def test_minMaxPromElement_stringElement(self):
         self.assertEqual(Estadistic().minMaxPromElement(" "), [0, None, None, None], "Empty string")
         self.assertEqual(Estadistic().minMaxPromElement(""), [0, None, None, None], "Empty string")
-#        self.assertEqual(Estadistic().minMaxElement("1"), [1, 1, 1], "One String numbers")
-#        self.assertEqual(Estadistic().minMaxElement("10"), [1, 10, 10], "One String numbers")
+        self.assertEqual(Estadistic().minMaxPromElement("1"), [1, 1, 1, 1], "One String numbers")
+        self.assertEqual(Estadistic().minMaxPromElement("10"), [1, 10, 10, 10], "One String numbers")
 #        self.assertEqual(Estadistic().minMaxElement("4,2"),[2,2,4],"Two String numbers")
 #        self.assertEqual(Estadistic().minMaxElement("10,1200"), [2, 10,1200], "Two String numbers")
 #        self.assertEqual(Estadistic().minMaxElement("4,2,93,7,24,7,3,12"),[8, 2, 93],"n String numbers")
