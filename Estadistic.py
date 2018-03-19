5066dcd735f1f335db9f16648ff88b81e45778a4
juanSebastian <js.barragan796@uniandes.edu.co>
2018-03-18 22:03:40 -0500

    I3-P3 pasar test string dos elemento num,min max  (verde)

--- Estadistic.py
+++ Estadistic.py
@@ -11,6 +11,10 @@ class Estadistic:
             return [0, None, None]
         elif "," in string:
             numbers = np.array(string.split(",")).astype(np.int)
-            return [len(numbers),min(numbers)]
+            if numbers[0] > numbers[1]:
+                return [2,numbers[1],numbers[0]]
+            else:
+                return [2, numbers[0], numbers[1]]
+#            return [len(numbers),min(numbers)]
         else:
             return [1,int(string),int(string)]
