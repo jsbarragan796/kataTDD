46ea46fe746e1af5810c51c5e5323f1d882dcdc3
juanSebastian <js.barragan796@uniandes.edu.co>
2018-03-18 20:30:19 -0500

    Iteracion1-Paso3 Se crea el test string de 2 numeros (rojo)

--- estadisticTest.py
+++ estadisticTest.py
@@ -7,5 +7,7 @@ from Estadistic import Estadistic
 class EstadisticTest(TestCase):
     def test_nElements_stringVacio(self):
         self.assertEqual(Estadistic().elements(" "),0,"Empty string")
-    def test_nElements_stringVacio(self):
-        self.assertEqual(Estadistic().elements("1"), 1, " One String")
+    def test_nElements_stringUnElemento(self):
+        self.assertEqual(Estadistic().elements("1"), 1, " One String numbers")
+    def test_nElements_stringDosElementos(self):
+        self.assertEqual(Estadistic().elements("1,1"), 2, " Two String numbers")
