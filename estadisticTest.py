c7ceace91291f14324d7f8da74c543d31f4203ef
juanSebastian <js.barragan796@uniandes.edu.co>
2018-03-18 22:05:20 -0500

    I3-P4 cear test string n elementos num,min max  (Rojo)

--- estadisticTest.py
+++ estadisticTest.py
@@ -22,3 +22,5 @@ class EstadisticTest(TestCase):
         self.assertEqual(Estadistic().minMaxElement("10"), [1, 10, 10], "One String numbers")
         self.assertEqual(Estadistic().minMaxElement("4,2"),[2,2,4],"Two String numbers")
         self.assertEqual(Estadistic().minMaxElement("10,1200"), [2, 10,1200], "Two String numbers")
+        self.assertEqual(Estadistic().minMaxElement("4,2,93,7,24,7,3,12"),[8, 2, 93],"n String numbers")
+        self.assertEqual(Estadistic().minMaxElement("10,12,892,23591,8"), [5, 8, 23591], "n String numbers")
