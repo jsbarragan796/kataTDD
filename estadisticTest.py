161e671f4d06e4102adeb135eb2d5124ad379586
juanSebastian <js.barragan796@uniandes.edu.co>
2018-03-18 20:47:20 -0500

    Iteracion1-Paso4 Se crea el test string de n numeros (rojo)

--- estadisticTest.py
+++ estadisticTest.py
@@ -11,3 +11,8 @@ class EstadisticTest(TestCase):
         self.assertEqual(Estadistic().elements("1"), 1, " One String numbers")
     def test_nElements_stringDosElementos(self):
         self.assertEqual(Estadistic().elements("1,1"), 2, " Two String numbers")
+    def test_nElements_stringNElementos(self):
+        self.assertEqual(Estadistic().elements("1,1,3,2"), 4, " Four String numbers")
+        self.assertEqual(Estadistic().elements("1,1"), 2, " Two String numbers")
+        self.assertEqual(Estadistic().elements("1"), 1, " One String numbers")
+        self.assertEqual(Estadistic().elements("1,4,6,2,6,32,1"), 7, " Seven String numbers")
