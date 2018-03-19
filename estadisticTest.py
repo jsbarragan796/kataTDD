ed38f6a548103b2362c3e52e4017bb653e734f48
juanSebastian <js.barragan796@uniandes.edu.co>
2018-03-18 21:36:46 -0500

    I2-P4 crear test string n elementos numero y min de los elementos(rojo)

--- estadisticTest.py
+++ estadisticTest.py
@@ -17,7 +17,10 @@ class EstadisticTest(TestCase):
     def test_minElement_stringOneElement(self):
         self.assertEqual(Estadistic().minElement("1"),[1,1],"One String numbers")
         self.assertEqual(Estadistic().minElement("10"), [1, 10], "One String numbers")
-
     def test_minElement_stringTwoElement(self):
         self.assertEqual(Estadistic().minElement("4,2"),[2,2],"Two String numbers")
         self.assertEqual(Estadistic().minElement("10,1200"), [2, 10], "Two String numbers")
+
+    def test_minElement_stringNElement(self):
+        self.assertEqual(Estadistic().minElement("4,2,93,7,24,7,3,12"),[8,2],"n String numbers")
+        self.assertEqual(Estadistic().minElement("10,12,892,23591,8"), [5, 8], "Two String numbers")
