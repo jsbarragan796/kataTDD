38b63c67c312cbeb873405d086f42754c3f213d9
juanSebastian <js.barragan796@uniandes.edu.co>
2018-03-18 20:14:37 -0500

    Iteracion1-Paso2 Se crea el test string de 1 numero (Rjo)

--- estadisticTest.py
+++ estadisticTest.py
@@ -5,5 +5,7 @@ __author__ = 'Juan Sebastian Barragan'
 from Estadistic import Estadistic
 
 class EstadisticTest(TestCase):
-    def test_nElements(self):
+    def test_nElements_stringVacio(self):
         self.assertEqual(Estadistic().elements(" "),0,"Empty string")
+    def test_nElements_stringVacio(self):
+        self.assertEqual(Estadistic().elements("1"), 1, " One String")
