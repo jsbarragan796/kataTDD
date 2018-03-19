a809c729de6acaae2857aaafb3dd1846fb4c9df5
juanSebastian <js.barragan796@uniandes.edu.co>
2018-03-18 20:58:26 -0500

    Iteracion1-Paso4 Se hace refactoring del rest numero de elementos (azul)

--- estadisticTest.py
+++ estadisticTest.py
@@ -5,13 +5,9 @@ __author__ = 'Juan Sebastian Barragan'
 from Estadistic import Estadistic
 
 class EstadisticTest(TestCase):
-    def test_nElements_stringVacio(self):
-        self.assertEqual(Estadistic().elements(" "),0,"Empty string")
-    def test_nElements_stringUnElemento(self):
-        self.assertEqual(Estadistic().elements("1"), 1, " One String numbers")
-    def test_nElements_stringDosElementos(self):
-        self.assertEqual(Estadistic().elements("1,1"), 2, " Two String numbers")
     def test_nElements_stringNElementos(self):
+        self.assertEqual(Estadistic().elements(" "),0,"Empty string")
+        self.assertEqual(Estadistic().elements(""), 0, "Empty string")
         self.assertEqual(Estadistic().elements("1,1,3,2"), 4, " Four String numbers")
         self.assertEqual(Estadistic().elements("1,1"), 2, " Two String numbers")
         self.assertEqual(Estadistic().elements("1"), 1, " One String numbers")
