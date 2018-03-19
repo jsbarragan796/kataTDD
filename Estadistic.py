fa6a554a83d1ad7e68f2ce8b086e5d6b46276a21
juanSebastian <js.barragan796@uniandes.edu.co>
2018-03-18 22:06:14 -0500

    I3-P4 cear test string n elementos num,min max  (verde)

--- Estadistic.py
+++ Estadistic.py
@@ -11,10 +11,6 @@ class Estadistic:
             return [0, None, None]
         elif "," in string:
             numbers = np.array(string.split(",")).astype(np.int)
-            if numbers[0] > numbers[1]:
-                return [2,numbers[1],numbers[0]]
-            else:
-                return [2, numbers[0], numbers[1]]
-#            return [len(numbers),min(numbers)]
+            return [len(numbers), min(numbers), max(numbers)]
         else:
-            return [1,int(string),int(string)]
+            return [1, int(string), int(string)]
